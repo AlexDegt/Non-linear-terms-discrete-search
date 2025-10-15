Corrected data preparation and NMSE calculation

experiments/ls/forward_path.py
@@ -7,7 +7,6 @@ import torch
 import random
 import numpy as np
 from oracle import count_parameters
-from trainer import train
 from utils import dataset_prepare
 from scipy.io import loadmat
 from scipy.signal import get_window, welch
@@ -66,7 +65,7 @@ dtype = torch.complex128
 # Elements of train_slots_ind, test_slots_ind must be higher than 0 and lower, than slot_num
 # In full-batch mode train, validation and test dataset are the same.
 # In mini-batch mode validation and test dataset are the same.
-train_slots_ind, validat_slots_ind, test_slots_ind = range(1), range(1, 2), range(1, 2)
+train_slots_ind, validat_slots_ind, test_slots_ind = range(1), range(1), range(1)
 delay_d = 0
 
 # Size of blocks to divide whole signal into
@@ -75,15 +74,17 @@ block_size = config["block_size"]
 alpha = 0.0
 # Configuration file
 config_train = None
+# Flag, which shows whether to return reference signal or not
+return_ref = True
 # Input signal is padded with pad_zeros zeros at the beginning and ending of input signal.
 # Since each 1d convolution in model CVCNN makes zero-padding with int(kernel_size/2) left and right, then 
 # NO additional padding in the input batches is required.
-trans_len = int(len(delays) // 2)
+trans_len = max([abs(item) for sublist in delays for item in sublist])
 pad_zeros = trans_len
 # Channel to compensate: A or B
 channel = config["channel"]
-dataset = dataset_prepare(data_path, dtype, device, batch_size, block_size, slot_num, pad_zeros, 
-                    delay_d, train_slots_ind, validat_slots_ind, test_slots_ind, channel=channel)
+dataset, ref_signal = dataset_prepare(data_path, dtype, device, batch_size, block_size, slot_num, pad_zeros, 
+                    delay_d, train_slots_ind, validat_slots_ind, test_slots_ind, channel=channel, return_ref=return_ref) 
 
 train_dataset, validate_dataset, test_dataset = dataset
 
@@ -104,8 +105,7 @@ def batch_to_tensors(a):
     return x, d
 
 def complex_mse_loss(d, y, model):
-    # d = d[..., trans_len if trans_len > 0 else None: -trans_len if trans_len > 0 else None]
-    error = (d - y)
+    error = (d - y)#[..., trans_len if trans_len > 0 else None: -trans_len if trans_len > 0 else None]
     return error.abs().square().sum() #+ alpha * sum(torch.norm(p)**2 for p in model.parameters())
 
 def loss(model, signal_batch):
@@ -117,10 +117,13 @@ def loss(model, signal_batch):
 def nmse_fn(model, dataset):
     targ_pow, loss_val = 0, 0
     for batch in dataset:
-        _, d = batch_to_tensors(batch)
-        targ_pow += d.abs().square().sum()
+        # _, d= batch_to_tensors(batch)
+        # targ_pow += d.abs().square().sum()
+        # targ_pow += d[..., trans_len if trans_len > 0 else None: -pad_zeros if pad_zeros > 0 else None].abs().square().sum()
+        input_pow = np.sum(np.abs(ref_signal) ** 2)
         loss_val += loss(model, batch)
-    return 10.0 * torch.log10((loss_val) / (targ_pow)).item()
+    # return 10.0 * torch.log10((loss_val) / (targ_pow)).item()
+    return 10.0 * torch.log10((loss_val) / (input_pow)).item()
 
 def aclr_fn_torch(sig, f, fs=1.0, nfft=1024, window='blackman', nperseg=None, noverlap=None):
     """
@@ -175,7 +178,7 @@ def aclr_fn_torch(sig, f, fs=1.0, nfft=1024, window='blackman', nperseg=None, no
     return aclr.item()  # Return as a scalar
 
 def load_weights(path_name, device=device):
-    return torch.load(path_name, map_location=torch.device(device))
+    return torch.load(path_name, map_location=torch.device(device), weights_only=True)
 
 def set_weights(model, weights):
     model.load_state_dict(weights)
@@ -187,7 +190,7 @@ def get_nested_attr(module, names):
             return
     return module
 
-model = ParallelCheby2D(order, delays, channel, dtype, device)
+model = ParallelCheby2D(order, delays, channel, dtype, device, trans_len)
 
 model.to(device)
 
@@ -213,16 +216,22 @@ with torch.no_grad():
         for j, batch in enumerate(dataset):
             data = batch_to_tensors(batch)
 
+            if channel == 'A':
+                channel_ind = 0
+            elif channel == 'B':
+                channel_ind = 1
+
             y.append(model(data[0])[0, 0, :])#[..., trans_len if trans_len > 0 else None: -trans_len if trans_len > 0 else None])
             d.append(data[1][0, 0, :])#[..., trans_len if trans_len > 0 else None: -trans_len if trans_len > 0 else None])
-            x.append(data[0][0, 0, trans_len if trans_len > 0 else None: -trans_len if trans_len > 0 else None])
+            x.append(data[0][0, channel_ind, trans_len if trans_len > 0 else None: -trans_len if trans_len > 0 else None])
 
         y_full_tensor = torch.cat(y, dim=-1).to(device)
         d_full_tensor = torch.cat(d, dim=-1).to(device)
         x_full_tensor = torch.cat(x, dim=-1).to(device)
-        y_full_numpy = y_full_tensor.detach().cpu().numpy()
-        d_full_numpy = d_full_tensor.detach().cpu().numpy()
-        x_full_numpy = x_full_tensor.detach().cpu().numpy()
+        y_full_numpy = y_full_tensor.detach().cpu().numpy() * 2 ** 15
+        d_full_numpy = d_full_tensor.detach().cpu().numpy() * 2 ** 15
+        x_full_numpy = x_full_tensor.detach().cpu().numpy() * 2 ** 15
+        # print(sum(abs(d_full_numpy)))
 
         aclr_val = aclr_fn_torch((x_full_tensor + d_full_tensor - y_full_tensor)[trans_len:-trans_len], f=f, fs=fs, nfft=nfft)
         aclr_val_dpdoff = aclr_fn_torch((x_full_tensor + d_full_tensor)[trans_len:-trans_len], f=f, fs=fs, nfft=nfft)

experiments/ls/run.py
@@ -61,7 +61,7 @@ dtype = getattr(torch, config["dtype"])
 # Elements of train_slots_ind, test_slots_ind must be higher than 0 and lower, than slot_num
 # In full-batch mode train, validation and test dataset are the same.
 # In mini-batch mode validation and test dataset are the same.
-train_slots_ind, validat_slots_ind, test_slots_ind = range(1), range(1, 2), range(1, 2)
+train_slots_ind, validat_slots_ind, test_slots_ind = range(1), range(1), range(1)
 delay_d = 0
 
 # Size of blocks to divide whole signal into
@@ -70,6 +70,8 @@ block_size = config["block_size"]
 alpha = 0.0
 # Configuration file
 config_train = None
+# Flag, which shows whether to return reference signal or not
+return_ref = True
 # Input signal is padded with pad_zeros zeros at the beginning and ending of input signal.
 # Since each 1d convolution in model CVCNN makes zero-padding with int(kernel_size/2) left and right, then 
 # NO additional padding in the input batches is required.
@@ -77,8 +79,8 @@ trans_len = max([abs(item) for sublist in delays for item in sublist])
 pad_zeros = trans_len
 # Channel to compensate: A or B
 channel = config["channel"]
-dataset = dataset_prepare(data_path, dtype, device, batch_size, block_size, slot_num, pad_zeros, 
-                    delay_d, train_slots_ind, validat_slots_ind, test_slots_ind, channel=channel) 
+dataset, ref_signal = dataset_prepare(data_path, dtype, device, batch_size, block_size, slot_num, pad_zeros, 
+                    delay_d, train_slots_ind, validat_slots_ind, test_slots_ind, channel=channel, return_ref=return_ref) 
 
 train_dataset, validate_dataset, test_dataset = dataset
 
@@ -114,11 +116,13 @@ def loss(model, signal_batch):
 def quality_criterion(model, dataset):
     targ_pow, loss_val = 0, 0
     for batch in dataset:
-        _, d= batch_to_tensors(batch)
+        # _, d= batch_to_tensors(batch)
         # targ_pow += d.abs().square().sum()
-        targ_pow += d[..., trans_len if trans_len > 0 else None: -pad_zeros if pad_zeros > 0 else None].abs().square().sum()
+        # targ_pow += d[..., trans_len if trans_len > 0 else None: -pad_zeros if pad_zeros > 0 else None].abs().square().sum()
+        input_pow = np.sum(np.abs(ref_signal) ** 2)
         loss_val += loss(model, batch)
-    return 10.0 * torch.log10((loss_val) / (targ_pow)).item()
+    # return 10.0 * torch.log10((loss_val) / (targ_pow)).item()
+    return 10.0 * torch.log10((loss_val) / (input_pow)).item()
 
 def load_weights(path_name, device=device):
     return torch.load(path_name, map_location=torch.device(device))

trainer/ols.py
@@ -4,6 +4,7 @@ from typing import Tuple, Union, Callable, List
 import numpy as np
 
 import itertools
+from itertools import product
 
 from .ls import train_ls
 
@@ -74,10 +75,13 @@ def train_ols(model: nn.Module, train_dataset: DataLoaderType, validate_dataset:
     if delays_range is None:
         delays_range = [-15, 15, 3]
 
-    comb_delays = list(itertools.combinations_with_replacement(np.arange(*delays_range), 3))
+    comb_delays = list(product(*[set(np.arange(*delays_range)) for _ in range(3)]))
+    # comb_delays = list(itertools.combinations_with_replacement(np.arange(*delays_range), 3))
     
     # Save all delays combinations
     np.save(os.path.join(save_path, "comb_delays.npy"), np.array(comb_delays))
+    # print(len(comb_delays))
+    # sys.exit()
 
     best_inds, best_delays = [], []
     # Initial terms matrix

utils/data_manage.py
@@ -41,7 +41,7 @@ class ResampleDataset(torch.utils.data.Dataset):
 def dataset_prepare(data_path: str, dtype: torch.dtype = torch.complex128, device: str = 'cuda', batch_size: OptionalInt = None, 
                     block_size: OptionalInt = None, slot_num: OptionalInt = None, pad_zeros: OptionalInt = None, 
                     delay_d: OptionalInt = None, train_slots_ind: range = range(1), validat_slots_ind: range = range(1),
-                    test_slots_ind: range = range(1), channel: str = 'A') -> DatasetType:
+                    test_slots_ind: range = range(1), channel: str = 'A', return_ref: bool = False) -> DatasetType:
     """
     The method extracts input and target data for the mat file, normalizes and resamples if necessary.
     Then it divides input and target tensors into the batches and loads them into the dataloader.
@@ -64,6 +64,7 @@ def dataset_prepare(data_path: str, dtype: torch.dtype = torch.complex128, devic
             A range with step 1. Defaults is range(1).
         test_slots_ind (range): Indices of the slots which are chosen for training dataset. A range with step 1. Defaults is range(1).
         channel (str): Channel to compensate non-linear distortion. Defaults to 'A'.
+        return_ref (bool): Flag, which shows whether to return reference signal or not.
 
     Returns:
         Tuple of iterables.
@@ -77,10 +78,15 @@ def dataset_prepare(data_path: str, dtype: torch.dtype = torch.complex128, devic
 
     mat = loadmat(data_path)
 
-    input_a = mat['PDinA'][0, :]
-    input_b = mat['PDinB'][0, :]
-    target_a = mat['PDoutA'][0, :] - mat['PDinA'][0, :]
-    target_b = mat['PDoutB'][0, :] - mat['PDinB'][0, :]
+    input_ref = mat['PDinB'][0, :].copy()
+    input_ref = input_ref[:len(input_ref) // 2] / (2 ** 15)
+    input_a = mat['PDinA'][0, :] / (2 ** 15)
+    input_b = mat['PDinB'][0, :] / (2 ** 15)
+    pa_out_a = mat['PDoutA'][0, :] / (2 ** 15)
+    pa_out_b = mat['PDoutB'][0, :] / (2 ** 15)
+
+    target_a = pa_out_a - input_a
+    target_b = pa_out_b - input_b
     nf = np.zeros_like(target_a)
 
     if delay_d is not None and delay_d != 0:
@@ -104,7 +110,9 @@ def dataset_prepare(data_path: str, dtype: torch.dtype = torch.complex128, devic
     nf = torch.tensor(nf, dtype=dtype).view(1, 1, -1).to(device)
 
     input /= input.abs().max()
-    target /= target.abs().max()
+    # print(target.size())
+    # print(target.abs().square().sum().item())
+    # target /= target.abs().max()
 
     assert (np.array(train_slots_ind) < slot_num).all() and (np.array(train_slots_ind) >= 0).all(), \
         "All train slots indices (argument train_slots_ind) must be positive and lower, than number of slots (argument slot_num)."
@@ -169,4 +177,8 @@ def dataset_prepare(data_path: str, dtype: torch.dtype = torch.complex128, devic
     test_set = torch.utils.data.DataLoader(test_set, batch_size=None)
     
     dataset.append(tuple((train_set, validat_set, test_set)))
-    return dataset[0]
+
+    if return_ref:
+        return dataset[0], input_ref
+    else:
+        return dataset[0]